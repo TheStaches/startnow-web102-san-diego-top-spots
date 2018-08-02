eb2ae85708e666a7a67e2cb56753c25854ac6895
Eric Dodds <ericdodds@Erics-MacBook-Air.local>
2018-08-02 15:02:31 -0700

    removed unnessecary code

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-// write your code here
     $.getJSON("data.json", function(data) {
         $.each(data, function(index, item) {
             var table = document.getElementById("myTable");
@@ -8,34 +7,13 @@ $(document).ready(function() {
             var hyperlink = "https://www.google.com/maps?q=" + item.location;
             var linkText = "<a href=" + hyperlink + "><button>Open in Google Maps</button></a>";
 
-
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
             var cell3 = row.insertCell(2);
 
-            
             cell1.innerHTML = item.name;
             cell2.innerHTML = item.description;
             cell3.innerHTML = linkText;
-            
-
-
         }); 
-        console.log(document.getElementById("myTable"));
     });
 });
-
-
-// // Find a <table> element with id="myTable":
-// var table = document.getElementById("myTable");
-
-// // Create an empty <tr> element and add it to the 1st position of the table:
-// var row = table.insertRow(0);
-
-// // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-// var cell1 = row.insertCell(0);
-// var cell2 = row.insertCell(1);
-
-// // Add some text to the new cells:
-// cell1.innerHTML = "NEW CELL1";
-// cell2.innerHTML = "NEW CELL2";
